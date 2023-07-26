1ec8acbc5c81eeeded7e7bedf44a45918888ced7
Aaron de los Santos <aaronjournalist@hotmail.com>
2023-07-26 22:46:25 +0200

    finish phase one

--- main.js
+++ main.js
@@ -1,25 +1,17 @@
 let enterUser = document.querySelector("#button_first_screen");
 const startGame = document.querySelector("#button_second_screen");
 const gameButton = document.querySelector("#button_game");
-const againGame = document.querySelector("#button_play_again");
-const watchOut = document.querySelector("#button_moving_second_screen");
-const movingButton = document.querySelector("#button_moving_game");
 const backLogin = document.querySelector("#button_back_login");
 
 let showLogin = document.querySelector(".insert_name");
-let showRanking = document.querySelector(".ranking");
 let showStartGame = document.querySelector(".pre_game-none");
 let showGame = document.querySelector(".main_screen_game-none");
 let showScore = document.querySelector(".score_screen-none");
-let showMovingStart = document.querySelector(".pre_moving_game-none");
-let showMovingGame = document.querySelector(".main_moving_screen_game-none");
 let showGetReady = document.querySelector(".ready_screen-none");
 
 let textScore = document.getElementById("your_score");
 let textUser = document.querySelector("#userName");
 let username = "";
-
-let beginChrono;
 
 //------------------------------LOGIN SCREEN--------------------------------//
 
@@ -47,7 +39,7 @@ startGame.addEventListener('click', ()=>{
     startTime = setTimeout(()=>{
         showGetReady.classList.add("ready_screen-none");
         showGetReady.classList.remove("ready_screen-flex");
-        showGame.classList.remove("main_screen_game-none")
+        showGame.classList.remove("main_screen_game-none");
         showGame.classList.add("main_screen_game-flex");
 
         startChrono();
@@ -56,8 +48,10 @@ startGame.addEventListener('click', ()=>{
 
 //------------------------------GAME BUTTON SCREEN--------------------------------//
 
+let beginChrono;
+
 function startChrono(){
-    beginChrono = Date.now()
+    beginChrono = Date.now();
 }
 
 let resultTime;
@@ -79,12 +73,6 @@ gameButton.addEventListener('click', ()=>{
 
 //--------------------------------SCORE SCREEN--------------------------------//
 
-againGame.addEventListener('click', ()=>{
-    showScore.classList.remove("score_screen");
-    showScore.classList.add("score_screen-none");
-    showMovingStart.classList.add("pre_moving_game-flex");
-} )
-
 backLogin.addEventListener('click', ()=>{
     showLogin.classList.add("insert_name");
     showLogin.classList.remove("insert_name-none");
@@ -103,7 +91,7 @@ updateHiresultList();
 function saveResult (username, result){
     const results = JSON.parse(localStorage.getItem("highTimeResults") || "[]");
     results.push({ username, result });
-    results.sort((a, b) => b.result - a.result);
+    results.sort((a, b) => a.result - b.result);
     localStorage.setItem("highTimeResults", JSON.stringify(results.slice(0, 10)));
     textScore.textContent =  "Your reaction time was " + resultTime;
 }
